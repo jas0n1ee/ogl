df6c767600e7455fdea066c447e7f994946ec506
jas0n1ee <i@jas0n1ee.me>
2019-03-03 21:54:49 +0800

    spinandsave

--- tutorial07_model_loading/tutorial07.cpp
+++ tutorial07_model_loading/tutorial07.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-
+#include <cstring>
 // Include GLEW
 #include <GL/glew.h>
 
@@ -20,7 +20,33 @@ using namespace glm;
 #include <common/controls.hpp>
 #include <common/objloader.hpp>
 
-int main( void )
+static GLubyte *pixels = NULL;
+static const GLenum FORMAT = GL_RGBA;
+static const GLuint FORMAT_NBYTES = 4;
+static const unsigned int HEIGHT = 768;
+static const unsigned int WIDTH = 1024;
+static unsigned int nscreenshots = 0;
+static unsigned int time;
+
+
+static void create_ppm(char *prefix, int frame_id, unsigned int width, unsigned int height,
+	unsigned int color_max, unsigned int pixel_nbytes, GLubyte *pixels) {
+	size_t i, j, k, cur;
+	enum Constants { max_filename = 256 };
+	char filename[max_filename];
+	snprintf(filename, max_filename, "%s%d.ppm", prefix, frame_id);
+	FILE *f = fopen(filename, "w");
+	fprintf(f, "P3\n%d %d\n%d\n", width, HEIGHT, 255);
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+			cur = pixel_nbytes * ((height - i - 1) * width + j);
+			fprintf(f, "%3d %3d %3d ", pixels[cur], pixels[cur + 1], pixels[cur + 2]);
+		}
+		fprintf(f, "\n");
+	}
+	fclose(f);
+}
+int main(int argc, char* argv[])
 {
 	// Initialise GLFW
 	if( !glfwInit() )
@@ -37,7 +63,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 07 - Model Loading", NULL, NULL);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Rayshaper Windows Player", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
@@ -62,7 +88,7 @@ int main( void )
     
     // Set the mouse at the center of the screen
     glfwPollEvents();
-    glfwSetCursorPos(window, 1024/2, 768/2);
+    glfwSetCursorPos(window, WIDTH /2, HEIGHT/2);
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -73,7 +99,7 @@ int main( void )
 	glDepthFunc(GL_LESS); 
 
 	// Cull triangles which normal is not towards the camera
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -84,32 +110,80 @@ int main( void )
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	int i = 1;
+	int frames = argc > 1 ? atoi(argv[1]) : 2147483647;
+	double lasttime = glfwGetTime();
+	GLuint vertexbuffer;
 
+
+	GLuint uvbuffer;
+	
+	pixels = new GLubyte[FORMAT_NBYTES * WIDTH * HEIGHT];
+	std::vector<GLuint> Texture;
+	std::vector<std::vector<glm::vec3>>vertices;
+	std::vector<std::vector<glm::vec2>> uvs;
+	std::vector<std::vector<glm::vec3>> normals;
+
+	glm::mat4 ViewMatrix;
+	glm::mat4 ProjectionMatrix;
+	glm::vec3 position = glm::vec3(0, 0, 0);
+	float FoV = 60;
+// Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Camera matrix
+	ViewMatrix = glm::lookAt(
+		position,           // Camera is here
+		position + glm::vec3(0,0,-1), // and looks here : at the same position, plus "direction"
+		glm::vec3(0,1,0)                  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+	for (int i = 0; i < frames; i++) {
+		char tex_filename[50];
+		sprintf(tex_filename, "%06d.bmp", i%frames + 1);
+		GLuint t_texture = loadBMP_custom(tex_filename);
+		if (!t_texture) {
+			frames = i;
+			break;
+		}
+		char obj_filename[50];
+		sprintf(obj_filename, "%06d.obj", i%frames + 1);
+		std::vector<glm::vec3> t_vertices;
+		std::vector<glm::vec2> t_uvs;
+		std::vector<glm::vec3> t_normals;
+		bool res = loadOBJ(obj_filename, t_vertices, t_uvs, t_normals);
+		if (!res) {
+			frames = i;
+			break;
+		}
+		Texture.push_back(t_texture);
+		vertices.push_back(t_vertices);
+		uvs.push_back(t_uvs);
+		normals.push_back(t_normals);
+	}
+	frames--;
+	glfwSetCursorPos(window, WIDTH / 2, HEIGHT / 2);
+	do {
 	// Load the texture
-	GLuint Texture = loadDDS("uvmap.DDS");
+//	GLuint Texture = loadDDS("uvmap.DDS");
+
 	
 	// Get a handle for our "myTextureSampler" uniform
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
 	// Read our .obj file
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec2> uvs;
-	std::vector<glm::vec3> normals; // Won't be used at the moment.
-	bool res = loadOBJ("cube.obj", vertices, uvs, normals);
+ // Won't be used at the moment.
 
 	// Load it into a VBO
 
-	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices[i%frames].size() * sizeof(glm::vec3), &vertices[i%frames][0], GL_STATIC_DRAW);
 
-	GLuint uvbuffer;
 	glGenBuffers(1, &uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
-
-	do{
+	glBufferData(GL_ARRAY_BUFFER, uvs[i%frames].size() * sizeof(glm::vec2), &uvs[i%frames][0], GL_STATIC_DRAW);
+	
+	
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -118,10 +192,27 @@ int main( void )
 		glUseProgram(programID);
 
 		// Compute the MVP matrix from keyboard and mouse input
-		computeMatricesFromInputs();
-		glm::mat4 ProjectionMatrix = getProjectionMatrix();
-		glm::mat4 ViewMatrix = getViewMatrix();
-		glm::mat4 ModelMatrix = glm::mat4(1.0);
+		//computeMatricesFromInputs();
+		//glm::mat4 ProjectionMatrix = getProjectionMatrix();
+		
+		//glm::mat4 ViewMatrix = getViewMatrix();
+		int speed_factor = 100;
+		float step = 2 * 3.1415 / speed_factor;
+		float x_scale = 0.2;
+		float y_scal2 = 0.2;
+		float x_offset = 0;
+		float y_offset = 0;
+		float z_offset = 0;
+		ViewMatrix = glm::lookAt(
+			glm::vec3(x_scale * cos((i % speed_factor)*step) + x_offset, 
+						y_scal2 * sin((i % speed_factor)*step) + y_offset, 
+						0 + z_offset),// Camera is here
+			glm::vec3(x_scale * cos((i % speed_factor)*step) + x_offset, 
+						y_scal2 * sin((i % speed_factor)*step) + y_offset,
+						0 + z_offset) + glm::vec3(0, 0, -1), // and looks here : at the same position, plus "direction"
+			glm::vec3(0, 1, 0)                  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+		glm::mat4 ModelMatrix = glm::mat4(1);
 		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
 		// Send our transformation to the currently bound shader, 
@@ -130,7 +221,7 @@ int main( void )
 
 		// Bind our texture in Texture Unit 0
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
+		glBindTexture(GL_TEXTURE_2D, Texture[i%frames]);
 		// Set our "myTextureSampler" sampler to use Texture Unit 0
 		glUniform1i(TextureID, 0);
 
@@ -159,7 +250,7 @@ int main( void )
 		);
 
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
+		glDrawArrays(GL_TRIANGLES, 0, vertices[i%frames].size() );
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -167,16 +258,24 @@ int main( void )
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
+		if (glfwGetTime() - lasttime > 0.03) {
+			lasttime = glfwGetTime();
+			i++;
+			glReadPixels(0, 0, WIDTH, HEIGHT, FORMAT, GL_UNSIGNED_BYTE, pixels);
+			create_ppm("tmp", nscreenshots, WIDTH, HEIGHT, 255, FORMAT_NBYTES, pixels);
+			nscreenshots++;
+		}
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
+	delete(pixels);
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
 	glDeleteProgram(programID);
-	glDeleteTextures(1, &Texture);
+	for(int i = 0; i < frames; i++)
+		glDeleteTextures(1, &Texture[i]);
 	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
